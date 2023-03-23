145f26976243f9a3ca80e22bdfe37329f415e733
DorianGray <doriansimon73@gmail.com>
2023-03-23 11:09:38 +0100

    feat(BDD): :sparkles: Ajout de la configuration de serveur

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
 const app = express();
+
+mongoose.connect('mongodb+srv://Judith:Test123@cluster0.o6sjw11.mongodb.net/?retryWrites=true&w=majority',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
+
 app.use(express.json());
 
 app.use((req, res, next) => {
