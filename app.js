222e8e957514320b1d0775cedaa7c12322d760f6
DorianGray <doriansimon73@gmail.com>
2023-03-30 09:54:31 +0200

    feat(app): :sparkles: Fin du cour

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
+const path = require('path');
 
 const stuffRoutes = require('./routes/stuff');
 const userRoutes = require('./routes/user');
@@ -24,5 +25,5 @@ app.use((req, res, next) => {
 
 app.use('/api/stuff', stuffRoutes);
 app.use('/api/auth', userRoutes);
-
+app.use('/images', express.static(path.join(__dirname, 'images')));
 module.exports = app;
